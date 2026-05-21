da2752883642adc3ad7546b14d6fd950b5ecf295
Prince John <madeitdao@gmail.com>
2026-05-21 03:23:27 +0100

    ux(landing): streamline hero and funnel order

--- frontend/src/app/(marketing)/page.tsx
+++ frontend/src/app/(marketing)/page.tsx
@@ -1,13 +1,13 @@
 import { HeroSection }        from "@/components/marketing/HeroSection"
 import { AiJudgeDemo }        from "@/components/marketing/AiJudgeDemo"
+import { SocialProofSection } from "@/components/marketing/SocialProofSection"
+import { CTASection }         from "@/components/marketing/CTASection"
 import { TwitterAnalyzer }    from "@/components/marketing/TwitterAnalyzer"
 import { ProblemSection }     from "@/components/marketing/ProblemSection"
 import { SolutionSection }    from "@/components/marketing/SolutionSection"
 import { FeaturesSection }    from "@/components/marketing/FeaturesSection"
 import { ComparisonSection }  from "@/components/marketing/ComparisonSection"
-import { SocialProofSection } from "@/components/marketing/SocialProofSection"
 import { FAQSection }         from "@/components/marketing/FAQSection"
-import { CTASection }         from "@/components/marketing/CTASection"
 import { DonateSection }      from "@/components/marketing/DonateSection"
 
 export default function LandingPage() {
@@ -15,15 +15,15 @@ export default function LandingPage() {
     <>
       <HeroSection />
       <AiJudgeDemo />
+      <SocialProofSection />
+      <CTASection />
       <TwitterAnalyzer />
       <ProblemSection />
       <SolutionSection />
       <FeaturesSection />
       <ComparisonSection />
-      <SocialProofSection />
       <FAQSection />
       <DonateSection />
-      <CTASection />
     </>
   )
 }
